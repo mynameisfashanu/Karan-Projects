326ca1bee1ee97e87dd82cfd4c3b286ed8e99adf
mynameisfashanu <fashanu13@gmail.com>
2017-10-10 22:31:33 +0200

    sovled problem3

---
+++ problem3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("largest prime factor: " + GetPrimeFactor());
+        }
+
+        public static bool IsPrime(long num)
+        {
+            for(long i = 2; i < num; i++)
+            {
+                if(num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public static long GetPrimeFactor()
+        {
+            long num = 600851475143; //600851475143;
+            long largest = 0;
+            for(long i = num; i > 1; i--)
+            {
+                if(num % i == 0)
+                {
+                    if(IsPrime(i))
+                    {
+                        largest = i;
+                        break;
+                    }
+                } 
+            }
+            return largest;
+        }
+    }
+}
