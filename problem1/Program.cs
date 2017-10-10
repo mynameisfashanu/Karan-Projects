ad2f1e23b9e31913cc5b75a607d4c29e6a7ee7db
mynameisfashanu <fashanu13@gmail.com>
2017-10-10 21:16:48 +0200

    solved problem 1

---
+++ problem1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int problem1(int count)
+        {
+            int sum = 0;
+            for(int i = 1; i < count; i++)
+            {
+                if(i % 5 == 0 || i % 3 == 0)
+                    sum += i;
+            }
+            return sum;
+        }
+
+    }
+}
