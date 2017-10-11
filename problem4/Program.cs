b7d9f418f0d8b08a7b93a57da9c95bd4ffe335b7
mynameisfashanu <fashanu13@gmail.com>
2017-10-11 21:35:51 +0200

    solved problem 4

---
+++ problem4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace problem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Largest Palindrome: " + LargestPalindrome());
+        }
+
+        public static bool IsPalindrom(int num)
+        {
+            string strnum = num.ToString();
+            string palindrome = "";
+            for(int i = strnum.Length - 1; i >= 0; i--)
+            {
+                palindrome += strnum[i];
+            }
+            if(palindrome == strnum)
+                return true;
+            return false;
+        }
+
+        /*
+            start from largest possible 3 digit multiple, 999 * 999 
+            subtract by 1, check if value is a palindrome.
+            if so start from 999 -> 100 , check if it has a 3 digit factor,
+            if so, divide the check if the result is also a 3 digit value,
+            if so  return the number.
+         */
+         public static int LargestPalindrome()
+         {
+             for(int i = 998001; i >= 10000; i--)
+             {
+                 if(IsPalindrom(i))
+                 {
+                     for(int j = 999; j >= 100; j--)
+                     {
+                         if(i % j == 0)
+                         {
+                             int value = (i / j);
+                             if(value.ToString().Length == 3)
+                                return i;
+                         }
+                     }
+                 }
+             }
+             return 0; 
+         }
+    }
+}
